6698376c211fb78802c921bebd2f7ea48cfc9f12
Victor Araujo <victorwaraujo@gmail.com>
2024-02-22 05:41:46 -0300

    incrementando o menu com js

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const menuOpener = document.querySelector('.menu-opener');
+    const menu = document.querySelector('.menu');
+    const nav = document.querySelector('nav');
+  
+    menuOpener.addEventListener('click', function() {
+      nav.classList.toggle('active');
+    });
+  });
